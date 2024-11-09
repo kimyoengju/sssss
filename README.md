5404b8bc7f18724bc95372d1e0ce1c15a275a9e8
kimyoengju <jhpower9977@naver.com>
2024-11-09 17:51:44 +0900

    https://github.com/kimyoengju/react241109.git

---
+++ README.md
@@ -0,0 +1,18 @@
+## node JS
+
+자바스크립트 실행 프로그램
+Javascript는 브라우저에서 동작했지만, 
+Node.js 덕분에 브라우저 밖에서도 Javascript 사용 가능
+
+---
+
+## npm
+
+npm : Node Package Manager
+Javascript 프로젝트에 필요한 패키지를 설치하고 관리하는데 사용
+
+npm start 
+
+npx : 일회성 실행 - 설치하지 않고 패키지를 바로 실행할 때 사용
+
+npx create-react-app@latest 폴더명
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Step01 from './step/step01';
+import Step02 from './step/step02';
+import Step03 from './step/step03';
+import Step04 from './step/step04';
+import Step05 from './step/step05';
+import Step06 from './step/step06';
+import Step07 from './step/step07';
+import Step08 from './step/step08';
+
+function App() {
+  return (
+    <>
+      {/* 컴포넌트 : 재사용 가능한 코드 조각(버튼, 텍스트, 이미지 등등...) */}
+      {/* <Comp /> */}
+      {/* <Comp /> */}
+      {/* <Step01 /> */}
+      {/* <Step02 /> */}
+      {/* <Step03 /> */}
+      {/* <Step04 title='게시판 제목' content='게시판내용' name='작성자' red='red' /> */}
+      {/* <Step05 /> */}
+      {/* <Step06 /> */}
+      {/* <Step07 /> */}
+      <Step08 />
+    </>
+  );
+}
+
+// 작명시 영어대문자로 시작
+// 함수표현식, 함수선언식 둘 다 가능
+
+function Comp() {
+  return <div>컴포넌트</div>
+}
+
+export default App;
